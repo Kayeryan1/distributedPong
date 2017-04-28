88912f39e8c9552987d350c039ae2824d916cec8
Michael <mpcarolin@gmail.com>
2017-04-27 21:03:18 -0700

    Finished making basic GUI prototype

--- src/GUI/ClientGUI.java
+++ src/GUI/ClientGUI.java
@@ -3,8 +3,6 @@ package GUI;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
 
 public class ClientGUI extends Application {
@@ -12,21 +10,33 @@ public class ClientGUI extends Application {
 	private int playerNumber = 1;
 	private final static int WINDOW_WIDTH = 500;
 	private final static int WINDOW_HEIGHT = 420;
-	private final static int PADDLE_LENGTH = 25; 
-	private final static int PADDLE_WIDTH = 3; 
-	private final static int PADDLE_PADDING = 15;
+	final static int PADDLE_LENGTH = 35; 
+	final static int PADDLE_WIDTH = 3; 
+	private final static int PADDLE_PADDING = 15;	// margin between screen edge and paddles
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("Distributed Pong –– Player " + playerNumber);
 		
-		Paddle paddleOne = new Paddle(PADDLE_PADDING, WINDOW_HEIGHT/2, PADDLE_LENGTH, PADDLE_WIDTH, PaddleOrientation.Vertical);
-		Paddle paddleTwo = new Paddle(WINDOW_WIDTH-PADDLE_PADDING, WINDOW_HEIGHT/2, PADDLE_LENGTH, PADDLE_WIDTH, PaddleOrientation.Vertical);
+		Paddle paddleOne = new Paddle(
+				PADDLE_PADDING, 					// x position
+				WINDOW_HEIGHT/2, 					// y position
+				PaddleOrientation.Vertical, 		// orientation (determines which edge on screen to position at)
+				true								// if true, this is the machine's player's paddle
+		);
+		Paddle paddleTwo = new Paddle(
+				WINDOW_WIDTH-PADDLE_PADDING, 
+				WINDOW_HEIGHT/2, 
+				PaddleOrientation.Vertical, 
+				false
+		);
 
 		Pane root = new Pane();
 		root.getChildren().addAll(paddleOne, paddleTwo);
+
 		primaryStage.setScene(new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT));
 		primaryStage.show();
+
 		paddleOne.requestFocus();
 	}
 	
--- src/GUI/Paddle.java
+++ src/GUI/Paddle.java
@@ -1,31 +1,29 @@
 package GUI;
 
 import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 
 class Paddle extends Line {
 	private final int width, length;
 	private final PaddleOrientation orientation;
 	private final Point position;
+	private boolean isPlayer;
 	
-	private static final int MOVE_DELTA = 10;
+	private static final int MOVE_DELTA = 15;	// units the paddle moves when a valid direction key is pressed
 
-	public Paddle(int x, int y, int length, int width, PaddleOrientation orientation) {
+	public Paddle(int x, int y, PaddleOrientation orientation, boolean isPlayer) {
 		super();
 		this.position = new Point(x, y);
-		this.width = width;
-		this.length = length;
+		this.width = ClientGUI.PADDLE_WIDTH;
+		this.length = ClientGUI.PADDLE_LENGTH;
 		this.orientation = orientation;
-
 		this.setStrokeWidth(width);
-
-		/* Key listener that will, on a directional key press, move the paddle */
-		this.setOnKeyPressed((event) -> {
-			KeyCode direction = event.getCode();
-			if (orientation.contains(direction)) {
-				move(direction);
-			}
-		});
+		if (isPlayer) {
+			setAsPlayer();
+		} else {
+			setAsOpponent();
+		}
 
 		update();
 	}
@@ -58,6 +56,26 @@ class Paddle extends Line {
 		case RIGHT:
 			move(position.x + MOVE_DELTA, position.y);
 			break;
+		default:
+			break;
 		}
+	}
+
+	private void setAsPlayer() {
+		this.isPlayer = true;
+		this.setStroke(Color.GREEN);
+
+		/* Key listener that will, on a directional key press, move the paddle */
+		this.setOnKeyPressed((event) -> {
+			KeyCode direction = event.getCode();
+			if (orientation.contains(direction)) {
+				move(direction);
+			}
+		});
+	}
+	
+	private void setAsOpponent() {
+		this.isPlayer = false;
+		this.setStroke(Color.RED);
 	}
 }
