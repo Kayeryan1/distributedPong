f10c3375cdf15488ba4b26b054a7f74c64eb0746
Michael <mpcarolin@gmail.com>
2017-04-27 19:50:30 -0700

    Orientation functions

--- src/GUI/ClientGUI.java
+++ src/GUI/ClientGUI.java
@@ -20,8 +20,8 @@ public class ClientGUI extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("Distributed Pong –– Player " + playerNumber);
 		
-		Paddle paddleOne = new Paddle(PADDLE_PADDING, WINDOW_HEIGHT/2, PADDLE_LENGTH, PADDLE_WIDTH);
-		Paddle paddleTwo = new Paddle(WINDOW_WIDTH-PADDLE_PADDING, WINDOW_HEIGHT/2, PADDLE_LENGTH, PADDLE_WIDTH);
+		Paddle paddleOne = new Paddle(PADDLE_PADDING, WINDOW_HEIGHT/2, PADDLE_LENGTH, PADDLE_WIDTH, Orientation.Vertical);
+		Paddle paddleTwo = new Paddle(WINDOW_WIDTH-PADDLE_PADDING, WINDOW_HEIGHT/2, PADDLE_LENGTH, PADDLE_WIDTH, Orientation.Vertical);
 
 		Pane root = new Pane();
 		root.getChildren().addAll(paddleOne, paddleTwo);
@@ -36,13 +36,15 @@ public class ClientGUI extends Application {
 
 class Paddle extends Line {
 	private final int width, length;
-	private Point position;
+	private final Orientation orientation;
+	private final Point position;
 
-	public Paddle(int x, int y, int length, int width) {
+	public Paddle(int x, int y, int length, int width, Orientation orientation) {
 		super();
 		this.position = new Point(x, y);
 		this.width = width;
 		this.length = length;
+		this.orientation = orientation;
 
 		this.setStrokeWidth(width);
 		update();
@@ -54,8 +56,29 @@ class Paddle extends Line {
 		this.setEndX(position.x);
 		this.setEndY(position.y + (length/2));
 	}
+	
+	private void setupListener() {
+		this.setOnKeyPressed((event) -> {
+			if (this.orientation == Orientation.Vertical) {
+				switch (event.getCode()) {
+				case UP:
+					break;
+				case DOWN:
+					break;
+				}
+			} else {
+				switch (event.getCode()) {
+				case LEFT:
+					break;
+				case RIGHT:
+					break;
+				}
+			}
+		});
+	}
 }
 
-class PaddleMover extends KeyListener {
-	
+enum Orientation {
+	Vertical, Horizontal;
 }
+
