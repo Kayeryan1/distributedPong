be6df744684164e9c7082ff8e5e1c14b59ef73df
Michael <mpcarolin@gmail.com>
2017-04-30 14:02:41 -0700

    work plz

--- src/network/ClientNetworkService.java
+++ src/network/ClientNetworkService.java
@@ -6,15 +6,17 @@ public class ClientNetworkService extends NetworkService {
 		super(hostAddress, hostPort, numPlayers);
 		// instantiate and connect with server
 		remotePlayerSockets[0] = new ClientSocket(this.hostAddress, this.hostPort, 0);
-		remotePlayerNetworkData[0] = new PlayerNetworkData(0, this.hostAddress, this.hostPort);
+		playerNetworkData[0] = new PlayerNetworkData(0, this.hostAddress, this.hostPort);
 		
 		// wait for network data
-		for (int i = 1; i <= this.numOpponents; i++) {
+		for (int i = 0; i <= this.numPlayers; i++) {
 			PlayerNetworkData data = remotePlayerSockets[0].receiveData();
-			remotePlayerNetworkData[i] = data;
 			if (data.address.equals(remotePlayerSockets[0].getLocalAddress())) {
 				this.localPlayerID = data.playerNumber;
+			} else if (i != 0) {
+				remotePlayerSockets[i] = new ClientSocket(data.address, data.port, data.playerNumber);
 			}
+			playerNetworkData[i] = data;
 		}
 	}
 }
